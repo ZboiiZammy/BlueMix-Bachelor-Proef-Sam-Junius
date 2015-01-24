befd07ba0e223470f8ec30a5a4a9f0342e12bbbb
Sam Junius <sam.junius@gmail.com>
2015-01-24 12:24:00 +0100

    Create README.m

---
+++ README.m
@@ -0,0 +1,29 @@
+#IBM BlueMix : The Mobile Cloud
+
+######*Bachelor Proef door Sam Junius*
+
+##Doelstelling
+
+In deze bachelorproef werk je een beperkte applicatie op het IBM BlueMix platform, meer bepaald met de boilerplate “Mobile Cloud”. Je onderzoekt hierbij de voor- en nadelen van dit RAD-Cloud-framework (Rapid Application Development) van IBM. Je zal je toespitsen op het Mobile Cloud gedeelte, slechts een onderdeel van BlueMix dat een heel scala aan ontwikkelingsmogelijkheden aanbiedt.
+
+##Inhoud
+
+BlueMix is een implementatie van de Open Cloud Architecture van IBM, waarbij ontwikkelaars in staat worden gesteld om op korte tijd kwaliteitsvolle applicaties te creëren, deployen en nadien ook te beheren. Hierbij biedt IBM een breed scala aan services en technologieën aan, die je naar hartenlust kan combineren. Dankzij het brede portfolio dat IBM via BlueMix aanbiedt is er voor nagenoeg elke applicatie een combinatie voorhanden.
+
+In deze bachelorproef zal je je specifiek verdiepen in de zogenaamde “Mobile Cloud” boilerplate. Dit is een combinatie van onderstaande technologieën en services:
+* **SDK for Node.js** geeft de mogelijkheid om mobiele en webapplicaties te maken door gebruik te maken van de mobile app template en SDK voor Node.js
+*	**Mobile Application Security** laat toe om de toegang tot de mobile cloud services te gaan restricteren en beveiligen.
+*	**Push** is een component waarmee je eenvoudig push-berichten naar mobiele toestellen kan sturen
+*	**Mobile Data** biedt SDK’s aan om binnen Android, iOS en JavaScript de back-end aan te spreken op een zeer transparante en eenvoudige manier.
+
+De Mobile Cloud boilerplate wordt meteen in een reële case toegepast. Je zal een applicatie ontwikkelen die de uitwisseling van bedrijfskennis mogelijk maakt. Een gebruiker kan hierbij een nieuwe best practice delen met collega’s omtrent bepaalde topics. Wanneer een werknemer ingeschreven is op een topic zal hij/zij een notification ontvangen wanneer nieuwe best practices werden toegevoegd in deze categorie. Aangezien het om bedrijfsgevoelige info gaat is beveiliging een must.
+
+##Eindresultaten
+
+Het eindresultaat van deze bachelorproef bestaat uit volgende onderdelen:
+* een kritische analyse van het BlueMix platform, meer bepaald de Mobile Cloud boilerplate
+* een technische uiteenzetting van de mogelijkheden van de Mobile Cloud boilerplate
+* een uitgewerkte applicatie (zie inhoud) met behulp van Mobile Cloud in BlueMix:
+  * Node.js
+  * iOS
+  * Android
